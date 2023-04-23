566a94b52f17e1e0eb1d7c7930c58d3adc12cde1
huitoutunao <lh_huitoutunao@163.com>
2023-04-23 17:05:11 +0800

    feat: 新增create-vue3-js

---
+++ create-vue3-js/src/stores/counter.js
@@ -0,0 +1,17 @@
+import { ref, computed } from 'vue'
+import { defineStore } from 'pinia'
+
+export const useCounterStore = defineStore('counter', () => {
+  const count = ref(0)
+  const doubleCount = computed(() => count.value * 2)
+  function increment() {
+    count.value += 1
+  }
+
+  return { count, doubleCount, increment }
+})
+
+export const useHelloWorld = defineStore('sayHi', () => {
+  const msg = 'hello world'
+  return { msg }
+})

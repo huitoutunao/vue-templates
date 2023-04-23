50cf475ee4051693f17a97eb9af3fc50335f335b
huitoutunao <lh_huitoutunao@163.com>
2023-04-23 17:48:13 +0800

    feat: 新增eslint

--- create-vue3-js/vite.config.js
+++ create-vue3-js/vite.config.js
@@ -2,10 +2,16 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import eslintPlugin from 'vite-plugin-eslint'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    eslintPlugin({
+      cache: true,
+    }),
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),

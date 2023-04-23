48d9d1443abf220a2395a46c8268d3d54fac4ebe
huitoutunao <lh_huitoutunao@163.com>
2023-04-23 23:54:17 +0800

    feat: 新增create-vue3-ts

---
+++ create-vue3-ts/.eslintrc.cjs
@@ -0,0 +1,26 @@
+require('@rushstack/eslint-patch/modern-module-resolution')
+
+const path = require('node:path')
+const createAliasSetting = require('@vue/eslint-config-airbnb-with-typescript/createAliasSetting')
+
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    node: true,
+    es2022: true,
+  },
+  extends: [
+    'plugin:vue/vue3-recommended',
+    '@vue/eslint-config-airbnb-with-typescript',
+    '@vue/eslint-config-prettier',
+  ],
+  rules: {
+    'no-plusplus': ['error', { allowForLoopAfterthoughts: true }],
+  },
+  settings: {
+    ...createAliasSetting({
+      '@': `${path.resolve(__dirname, './src')}`,
+    }),
+  },
+}

677cbfbc73005dbd56edc4093b9388e0cf7146a8
huitoutunao <lh_huitoutunao@163.com>
2023-06-04 00:32:55 +0800

    feat: 新增vue3-cli-ts

---
+++ vue3-cli-ts/.eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    node: true,
+  },
+  extends: [
+    'plugin:vue/vue3-essential',
+    '@vue/airbnb',
+    '@vue/typescript/recommended',
+    '@vue/eslint-config-prettier',
+  ],
+  parserOptions: {
+    ecmaVersion: 2020,
+  },
+  rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+  },
+}
---
+++ vue3-cli-ts/src/views/HomeView.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+    <button @click="sayWord">点我sayWord</button>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from 'vue'
+import HelloWorld from '@/components/HelloWorld.vue' // @ is an alias to /src
+
+export default defineComponent({
+  name: 'HomeView',
+  components: {
+    HelloWorld,
+  },
+  setup() {
+    const word = ref('hello world')
+    const sayWord = () => {
+      console.log(word.value)
+    }
+
+    return {
+      sayWord,
+    }
+  },
+})
+</script>

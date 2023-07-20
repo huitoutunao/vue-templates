1746409c790d5d302eb0cfc60b0f13490e95e82e
huitoutunao <lh_huitoutunao@163.com>
2023-07-21 00:54:45 +0800

    feat: 新增模板

---
+++ create-vue3-ts-official/.eslintrc.cjs
@@ -0,0 +1,25 @@
+/* eslint-env node */
+require('@rushstack/eslint-patch/modern-module-resolution')
+
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    node: true,
+    es2022: true,
+  },
+  extends: [
+    'plugin:vue/vue3-essential',
+    'eslint:recommended',
+    '@vue/eslint-config-typescript',
+    '@vue/eslint-config-prettier',
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+  },
+  rules: {
+    'no-var': 'error',
+    'no-unused-vars': 'error',
+    'prefer-const': 'error',
+  },
+}

b65e711dc0d2661e6f6a1073028eb10606e9ba5a
huitoutunao <lh_huitoutunao@163.com>
2023-11-13 10:54:57 +0800

    feat: 更新create-vue3-ts-official

--- create-vue3-ts-official/.eslintrc.cjs
+++ create-vue3-ts-official/.eslintrc.cjs
@@ -3,19 +3,14 @@ require('@rushstack/eslint-patch/modern-module-resolution')
 
 module.exports = {
   root: true,
-  env: {
-    browser: true,
-    node: true,
-    es2022: true,
-  },
   extends: [
     'plugin:vue/vue3-essential',
     'eslint:recommended',
     '@vue/eslint-config-typescript',
-    '@vue/eslint-config-prettier',
+    '@vue/eslint-config-prettier/skip-formatting'
   ],
   parserOptions: {
-    ecmaVersion: 'latest',
+    ecmaVersion: 'latest'
   },
   rules: {
     'no-var': 'error',
--- create-vue3-ts-official/README.md
+++ create-vue3-ts-official/README.md
@@ -1,4 +1,4 @@
-# create-vue3-ts-official
+# vue3
 
 This template should help get you started developing with Vue 3 in Vite.
 
